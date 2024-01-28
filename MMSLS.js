8d2b6f1c25a6473185893cbf094e69aca32ee4c5
XIANG <108182531+tc507886067@users.noreply.github.com>
2024-01-28 21:22:11 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,8 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=14053084&pid=9cf76b575daec2549dfcf24c634ad51b&tid=42114c2c140cd30f37d030f2a11f20ba",
-    "https://www.maimemo.com/page?sid=24e302acc13c5740f1b5bbcd75a1c57b&uid=33752068&pid=9cf76b575daec2549dfcf24c634ad51b&tid=69a7b42a049b9a00947a43e4badce38c&view_time=1706068990&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit-EZNAr0bVnRjT-tYvfkL1Q&token=46161c4363fb5454920b7a963f7a81c70bad727c6f43cf3df2f0a7e201061967&expired_time=2025-01-23T12:03:10%2008:00",
-
+    "https://www.maimemo.com/share/page?uid=14053084&pid=efb3c256029b3b2c05b34c3cdba56e53&tid=97bc638e04fda1f66b837bb68f30c46f",
+   
   ]
   /**
    * 生成随机数字
